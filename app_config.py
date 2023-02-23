69d4a0fb232d6d50ecabfddee3e4bc884e407a88
Dalton Tofoli <daltontofoli1@gmail.com>
2023-02-23 14:46:17 -0300

    removed sensible data from config file

--- app_config.py
+++ app_config.py
@@ -1,11 +1,5 @@
 import os
 
-CLIENT_ID = "98777980-eca9-4b98-b037-f400cfde6517"
-
-CLIENT_SECRET = "M.28Q~qufrrA0G3sNE4fd5hYm_VgtFK0DSvrRcjx"
-
-AUTHORITY = "https://login.microsoftonline.com/6f7e4eb7-c839-418d-9308-7fdc660bebeb"
-
 SCOPE = ["User.ReadBasic.All"]
 
 ENDPOINT = "https://graph.microsoft.com/v1.0/me"
